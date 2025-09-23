84691a8722af372ce4a90a43866402797b876e20
maezti <marie.edet01@gmail.com>
2025-09-23 16:25:56 +0200

    add features extraction

--- fall_features_extraction.py
+++ fall_features_extraction.py
@@ -1,52 +1,46 @@
 # Feature Extraction for Falls
-
+from src.extract_landmark import video_to_array
 import numpy as np
 
-# input : array (#frame, #landmarks, #coordonnees)
 
-
-# output : array (#frame, #features, acceleration (#coordonnees), vitesse (#coordonnees), etat)
-# landmarks selectionnes : nez, centre épaules, centre hanche, ankle droite, ankle gauche, poignet droite, poignet gauche
-
-
-def table_features(landmarks):
-    nb_frames, nb_landmarks, nb_coordonnees = np.shape(landmarks)
+# def table_features(landmarks):
+#     nb_frames, nb_landmarks, nb_coordonnees = np.shape(landmarks)
     
-    nb_landmarks_selectionnes = 7
+#     nb_landmarks_selectionnes = 7
     
-    features = np.zeros((nb_frames, nb_landmarks_selectionnes, nb_coordonnees * 3 + 2)) # 3 : vecteurs des : coordonnees (0,1,2), vitesse(3,4,5)+6, acce (6,7,8)+9
+#     features = np.zeros((nb_frames, nb_landmarks_selectionnes, nb_coordonnees * 3 + 2)) # 3 : vecteurs des : coordonnees (0,1,2), vitesse(3,4,5)+6, acce (6,7,8)+9
 
-    ## COORDONNEES
-    # nez
-    features[:, 0, 0:3] = landmarks[:, 0, :3]
+#     ## COORDONNEES
+#     # nez
+#     features[:, 0, 0:3] = landmarks[:, 0, :3]
 
-    # centre épaules
-    features[:, 1, 0:3] = np.mean(landmarks[:, 11:13, :3], axis=1)
+#     # centre épaules
+#     features[:, 1, 0:3] = np.mean(landmarks[:, 11:13, :3], axis=1)
 
-    # centre hanches
-    features[:, 2, 0:3] = np.mean(landmarks[:, 23:25, :3], axis=1)
+#     # centre hanches
+#     features[:, 2, 0:3] = np.mean(landmarks[:, 23:25, :3], axis=1)
 
-    # poignets
-    features[:, 3, 0:3] = landmarks[:, 15, :3]
-    features[:, 4, 0:3] = landmarks[:, 16, :3]
+#     # poignets
+#     features[:, 3, 0:3] = landmarks[:, 15, :3]
+#     features[:, 4, 0:3] = landmarks[:, 16, :3]
 
-    # chevilles
-    features[:, 5, 0:3] = landmarks[:, 27, :3]
-    features[:, 6, 0:3] = landmarks[:, 28, :3]
+#     # chevilles
+#     features[:, 5, 0:3] = landmarks[:, 27, :3]
+#     features[:, 6, 0:3] = landmarks[:, 28, :3]
 
-    ## V & A
-    dt = 1/24 # temps entre 2 frames
-    for idx_landmarks in range(7):
-        # VITESSE 
-        velocity = np.diff(features[:, idx_landmarks, 0:3], axis=0) / dt
-        features[1:, idx_landmarks, 3:6] = velocity
-        features[1:, idx_landmarks, 6] = np.linalg.norm(velocity, axis=1)
-        # ACCELERATION
-        acceleration = np.diff(features[:, idx_landmarks, 4:7], axis=0) / dt
-        features[2:, idx_landmarks, 7:10] = acceleration
-        features[2:, idx_landmarks, 10] = np.linalg.norm(acceleration, axis=1)
+#     ## V & A
+#     dt = 1/24 # temps entre 2 frames
+#     for idx_landmarks in range(7):
+#         # VITESSE 
+#         velocity = np.diff(features[:, idx_landmarks, 0:3], axis=0) / dt
+#         features[1:, idx_landmarks, 3:6] = velocity
+#         features[1:, idx_landmarks, 6] = np.linalg.norm(velocity, axis=1)
+#         # ACCELERATION
+#         acceleration = np.diff(features[:, idx_landmarks, 4:7], axis=0) / dt
+#         features[2:, idx_landmarks, 7:10] = acceleration
+#         features[2:, idx_landmarks, 10] = np.linalg.norm(acceleration, axis=1)
 
-    return features
+#     return features
 
 
 def cha_table_features(landmarks):
@@ -90,5 +84,9 @@ def cha_table_features(landmarks):
 
 
 if __name__ == "__main__":
-    landmarks = None
-    cha_table_features(landmarks)
+    video_path = "/home/marie.edet@Digital-Grenoble.local/Documents/mod18_acv/data/chute_ouvrier.mp4"
+    res  = video_to_array(video_path)
+    print(res.shape)
+    features = cha_table_features(res)
+    print(features.shape)
+    print(features[:, 0, :])
